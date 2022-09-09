3468e47e8e801eda0138244bf8a943740e23d922
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2022-09-09 12:40:23 +0700

    p

---
+++ Data.py
@@ -0,0 +1,58 @@
+#
+# Copyright (C) 2022-2023 by miskumis@Github, <https://github.com/miskumis >.
+
+
+from pyrogram.types import InlineKeyboardButton
+
+class Data:
+    HELP = """
+<b> ❏ Perintah untuk Pengguna BOT
+ ├ /start - Mulai Bot
+ ├ /about - Tentang Bot ini
+ ├ /help - Bantuan Perintah Bot ini
+ ├ /ping - Untuk mengecek bot hidup
+ └ /uptime - Untuk melihat status bot 
+ 
+ ❏ Perintah Untuk Admin BOT
+ ├ /logs - Untuk melihat logs bot
+ ├ /vars - Untuk melihat variable bot
+ ├ /setvar - Untuk mengatur var dengan command dibot
+ ├ /delvar - Untuk menghapus var dengan command dibot
+ ├ /getvar - Untuk melihat salah satu var dengan command dibot
+ ├ /users - Untuk melihat statistik pengguna bot
+ ├ /batch - Untuk membuat link lebih dari satu file
+ ├ /speedtest - Untuk Mengetes kecepatan server bot
+ └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
+
+👨‍💻 Develoved by @Gladesupportgroup</b>
+"""
+
+    close = [
+        [InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]
+    ]
+
+    mbuttons = [
+        [
+            InlineKeyboardButton("ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+        ],
+    ]
+
+    buttons = [
+        [
+            InlineKeyboardButton("ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ", callback_data="about"),
+            InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
+        ],
+    ]
+
+    ABOUT = """
+<b>Tentang Bot ini:
+
+@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+
+ • Creator: @{}
+
+ • Owner Repo: miss
+
+👨‍💻 Develoved by @Gladesupportgroup</b>
+"""
